d18ca809b8c2c3f1a6326f97f326a0237a3adb95
Orlando Grabiel Toledano Lopez <ogtoledano@uci.cu>
2022-03-07 18:38:57 -0500

    Add EMO EVAL dataset

--- utils/file_arguments_reader.py
+++ utils/file_arguments_reader.py
@@ -7,7 +7,7 @@ def load_param_from_file(url_param):
     for values in params:
         aux = values.split('=')
 
-        if aux[0] in ['dataset_train', 'dataset_test', 'dataset_dictionary', 'cnn_checkpoint', 'name_log_experiments_result','f_params_name','f_optimizer_name','f_history_name','cnn_optimizer','f_criterion_name','word_embedding_pretrained_glove_language']:
+        if aux[0] in ['dataset_train', 'dataset_test', 'dataset_dev', 'dataset_dictionary', 'cnn_checkpoint', 'name_log_experiments_result','f_params_name','f_optimizer_name','f_history_name','cnn_optimizer','f_criterion_name','word_embedding_pretrained_glove_language']:
             dictionary_param[aux[0]] = aux[1].split('\n')[0]
 
         if aux[0] in ['epochs','epochs_gs_cv','num_executions','cuda_device_id', 'generations', 'sgd_early_stopping_patientia', 'sgd_batch_size', 'grid_search_cross_val_cv', 'individuals', 'labels','word_embedding_size']:
