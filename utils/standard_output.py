f2a6ef82c693eccf53019111a8910b0cac4727d6
Orlando Grabiel Toledano Lopez <ogtoledano@uci.cu>
2022-03-25 20:51:13 -0400

    Add sentiment file score for two tasks/models

--- utils/standard_output.py
+++ utils/standard_output.py
@@ -21,3 +21,28 @@ def score_sentiment(X, model, device):
 
     return output
 
+
+def score_sentiment_two_task(X, model1,model2, device):
+    model1.to(device)
+    model1.eval()
+
+    model2.to(device)
+    model2.eval()
+
+    atractions = {0: "Hotel", 1: "Restaurant", 2: "Attractive"}
+
+    output = ""
+    with torch.no_grad():
+        for i,data in enumerate(X):
+            x_test = torch.from_numpy(data['features']).type(torch.LongTensor).to(device)
+            x_test = torch.reshape(x_test, (1, x_test.shape[0]))
+
+            prob1 = model1(x_test)
+            _, predicted1 = torch.max(prob1.data, 1)
+
+            prob2 = model2(x_test)
+            _, predicted2 = torch.max(prob2.data, 1)
+
+            output += "\"sentiment\"\t\"{}\"\t\"{}\"\t\"{}\"\n".format(i+1, predicted1.cpu().numpy()[0]+1, atractions[predicted1.cpu().numpy()[0]])
+
+    return output
